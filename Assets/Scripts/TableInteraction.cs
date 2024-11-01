9f6e826bed3d276b94f1dea14b2e6c0c8743a9dc
Zafkiel1312 <Timstar131220@gmail.com>
2024-11-01 18:12:22 +0100

    You spin me right round baby right round

--- Assets/Scripts/TableInteraction.cs
+++ Assets/Scripts/TableInteraction.cs
@@ -9,36 +9,44 @@ public class TableInteraction : MonoBehaviour
     public Transform interactorTransform;
     private float lastFrameAngle;
     public float multiplier = 1;
+    
+    private Vector3 lastPosition;
+    private Vector3 centerTable;
 
     private void Start() {
         XRGrabInteractable interactable = GetComponent<XRGrabInteractable>();
         interactable.selectEntered.AddListener(Selected);
         interactable.selectExited.AddListener(Deselected);
+        
+        centerTable = GetComponent<Renderer>().bounds.center;
     }
-
-    // private void Update()
-    // {
-    //     var inputDevices = new List<UnityEngine.XR.InputDevice>();
-    //     UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-    //
-    //     foreach (var device in inputDevices)
-    //     {
-    //         Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
-    //     }
-    // }
 
     private void FixedUpdate() {
         if (interactorTransform != null) {
-            float angle = Vector3.SignedAngle(interactorTransform.position - transform.position, interactorTransform.forward, Vector3.up);
-            float delta = angle - lastFrameAngle;
-            transform.Rotate(transform.up, delta*multiplier);           
-            lastFrameAngle = angle;
+            // float angle = Vector3.SignedAngle(interactorTransform.position - transform.position, interactorTransform.forward, Vector3.up);
+            // float delta = angle - lastFrameAngle;
+            // transform.Rotate(transform.up, delta*multiplier);           
+            // lastFrameAngle = angle;
+            
+            Vector3 interactorPosition = interactorTransform.position;
+            Vector3 vecA = centerTable - lastPosition;
+            Vector3 vecB = centerTable - interactorPosition;
+            float delta = Vector3.Angle(vecA, vecB);
+            float cross = Vector3.Cross(vecA, vecB).y;
+            float angle = delta;
+            if (cross < 0)
+            {
+                angle *= -1;
+            }
+            transform.Rotate(transform.up, angle * multiplier);
+            lastPosition = interactorPosition;
         }
     }
 
     public void Selected(SelectEnterEventArgs arguments) {        
         interactorTransform = arguments.interactorObject.transform;
         lastFrameAngle = Vector3.SignedAngle(interactorTransform.position - transform.position, interactorTransform.forward, Vector3.up);   
+        lastPosition = interactorTransform.position;
         Debug.Log("selected");
     }
 
