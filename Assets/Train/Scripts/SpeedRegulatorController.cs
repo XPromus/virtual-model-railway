e9be80ee6c8ce2ab07646abafb8187a772aa5e0d
Christopher Hilgner <hilgi45@gmail.com>
2024-11-19 16:46:47 +0100

    Train movement with speed regulator.

---
+++ Assets/Train/Scripts/SpeedRegulatorController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Train.Scripts
+{
+    public class SpeedRegulatorController : MonoBehaviour
+    {
+        [SerializeField] private float minEulerRotation = 0f;
+        [SerializeField] private float maxEulerRotation = 300f;
+        [SerializeField] private Transform knobTransform;
+        [SerializeField] private Transform baseTransform;
+
+        [SerializeField] private TrainController trainController;
+
+        private float _currentSpeedRegulatorPosition;
+        private float currentSpeedRegulatorPosition
+        {
+            get => _currentSpeedRegulatorPosition;
+            set
+            {
+                _currentSpeedRegulatorPosition = Mathf.Clamp01(value);
+                Debug.Log(_currentSpeedRegulatorPosition);
+                trainController.CurrentTrainSpeed = trainController.MaxTrainSpeed * _currentSpeedRegulatorPosition;
+            }
+        }
+
+        private void Update()
+        {
+            //var newRotation = new Vector3(-90f, ClampAngle(knobTransform.rotation.y, minEulerRotation, maxEulerRotation), 0f);
+            //knobTransform.rotation = Quaternion.Euler(newRotation);
+            currentSpeedRegulatorPosition = knobTransform.rotation.eulerAngles.y / maxEulerRotation;
+        }
+
+        private float ClampAngle(float angle, float min, float max)
+        {
+            if (angle < 0f) angle = 360f + angle;
+            return angle > 180f ? Mathf.Max(angle, 360f + min) : Mathf.Min(angle, max);
+        }
+    }
+}
---
+++ Assets/Train/Scripts/TrainController.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace Train.Scripts
+{
+    public class TrainController : MonoBehaviour
+    {
+        [SerializeField] private SplineContainer targetTrack;
+        public SplineContainer TargetTrack
+        {
+            get => targetTrack;
+            set => targetTrack = value;
+        }
+        
+        [SerializeField] private float maxTrainSpeed;
+        public float MaxTrainSpeed
+        {
+            get => maxTrainSpeed;
+            set => maxTrainSpeed = value;
+        }
+        
+        [SerializeField] private float currentTrainSpeed = 1f;
+        public float CurrentTrainSpeed
+        {
+            get => currentTrainSpeed;
+            set => currentTrainSpeed = value;
+        }
+        
+        [SerializeField] private float rotationSpeed = 1f;
+        
+        private float _currentDistance;
+
+        private void Start()
+        {
+            var firstKnot = TargetTrack.Spline.Knots.ToArray()[0];
+            transform.rotation = firstKnot.Rotation;
+            transform.position = TargetTrack.transform.TransformPoint(firstKnot.Position);
+        }
+
+        private void Update()
+        {
+            AnimateTrainAlongSpline();
+        }
+        
+        private void AnimateTrainAlongSpline()
+        {
+            var targetPosition = TargetTrack.EvaluatePosition(_currentDistance);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, currentTrainSpeed * Time.deltaTime);
+            var targetDirection = (Vector3) TargetTrack.EvaluateTangent(_currentDistance);
+            
+            if (targetDirection != Vector3.zero)
+            {
+                var targetRotation = Quaternion.LookRotation(targetDirection, transform.up);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+
+            if (_currentDistance >= 1f)
+            {
+                _currentDistance = 0f;
+            }
+            else
+            {
+                var splineLength = TargetTrack.CalculateLength();
+                var movement = currentTrainSpeed * Time.deltaTime / splineLength;
+                _currentDistance += movement;
+            }
+        }
+    }
+}
