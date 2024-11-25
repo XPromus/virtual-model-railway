7ce2f1ad0ddc6c73eb103927df33929ef4f52a5a
Zafkiel1312 <Timstar131220@gmail.com>
2024-11-26 00:01:55 +0100

    The rotary knob finally rotary knobs like a real rotary knob (as a rotary knob should)

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,4 @@
+﻿public enum Direction
+{
+    Foreward, Backward, Left, Right, Up, Down
+}
--- Assets/Scripts/TableInteraction.cs
+++ Assets/Scripts/TableInteraction.cs
@@ -6,45 +6,90 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class TableInteraction : MonoBehaviour
 {
-    public Transform interactorTransform;
     public float multiplier = 1;
-    
-    private Vector3 lastPosition;
-    private Vector3 centerTable;
+    public Direction direction = Direction.Up;
+    public bool clampRotation = false;
+    public float minRotation = 0;
+    public float maxRotation = 300;
+
+    private Transform _interactorTransform;
+    private Vector3 _lastPosition;
+    private Vector3 _centerTable;
 
     private void Start()
     {
         XRGrabInteractable interactable = GetComponent<XRGrabInteractable>();
         interactable.selectEntered.AddListener(Selected);
         interactable.selectExited.AddListener(Deselected);
-        
-        centerTable = GetComponent<Renderer>().bounds.center;
+
+        _centerTable = GetComponent<Renderer>().bounds.center;
     }
 
-    private void FixedUpdate() {
-        if (interactorTransform != null) {
-            Vector3 interactorPosition = interactorTransform.position;
-            Vector3 vecA = centerTable - lastPosition;
-            Vector3 vecB = centerTable - interactorPosition;
+    private void FixedUpdate()
+    {
+        if (_interactorTransform != null)
+        {
+            Vector3 interactorPosition = _interactorTransform.position;
+            Vector3 vecA = _centerTable - _lastPosition;
+            Vector3 vecB = _centerTable - interactorPosition;
             float delta = Vector3.Angle(vecA, vecB);
             float cross = Vector3.Cross(vecA, vecB).y;
             float angle = delta;
+            Vector3 rotationAxis = GetAxis(direction);
             if (cross < 0)
             {
                 angle *= -1;
             }
-            transform.Rotate(transform.up, angle * multiplier);
-            lastPosition = interactorPosition;
+
+            if (clampRotation)
+            {
+                if (GetRotatability(angle * multiplier))
+                    transform.Rotate(rotationAxis, angle * multiplier, Space.World);
+            }
+            else
+            {
+                transform.Rotate(rotationAxis, angle * multiplier, Space.World);
+            }
+
+            _lastPosition = interactorPosition;
         }
     }
 
-    private void Selected(SelectEnterEventArgs arguments) {        
-        interactorTransform = arguments.interactorObject.transform; 
-        lastPosition = interactorTransform.position;
+    private void Selected(SelectEnterEventArgs arguments)
+    {
+        _interactorTransform = arguments.interactorObject.transform;
+        _lastPosition = _interactorTransform.position;
         Debug.Log("selected");
     }
 
-    private void Deselected(SelectExitEventArgs arguments) {        
-        interactorTransform = null;        
+    private void Deselected(SelectExitEventArgs arguments)
+    {
+        _interactorTransform = null;
+    }
+
+    private Vector3 GetAxis(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Right: return transform.right;
+            case Direction.Left: return -transform.right;
+            case Direction.Up: return transform.up;
+            case Direction.Down: return -transform.up;
+            case Direction.Foreward: return transform.forward;
+            case Direction.Backward: return -transform.forward;
+        }
+
+        return transform.up;
+    }
+
+    private bool GetRotatability(float rotationAngle)
+    {
+        float currentRotation = Vector3.Scale(transform.rotation.eulerAngles, GetAxis(direction)).magnitude;
+        if (currentRotation + rotationAngle < minRotation || currentRotation + rotationAngle > maxRotation)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
